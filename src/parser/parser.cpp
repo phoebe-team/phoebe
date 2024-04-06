7e8fd0ca16e4bf7cf852743108df098c4e515131
jcoulter12 <jcoulter@flatironinstitute.org>
2024-04-06 09:15:38 -0400

    reorganize the file parser for phonon FC2s

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -2,22 +2,30 @@
 
 std::tuple<Crystal, PhononH0> Parser::parsePhHarmonic(Context &context) {
 
-  std::string fileName = context.getPhonopyDispFileName();
+  std::string fc2FileName = context.getPhFC2FileName();
 
-  // check if this file is set -- if it is, we read from phonopy
-  if (fileName.empty()) {
-    if(context.getPhFC2FileName().find("hdf5") != std::string::npos) {
-      // Note for the when someone forgets to set PhonopyDispFileName and gets an stoi error
-      if(mpi->mpiHead()) {
-        std::cout << "Warning: Reading in a QE .fc file with a name containing hdf5." << '\n' <<
-        "If you are trying to use Phonopy force constants," <<
-        " make sure to set the PhonopyDispFileName variable." << std::endl;
-        }
-    }
+  // try to use file extension to determine which file we have
+
+  // from qe we expect a seedname.fc file
+  if(fc2FileName.find(".fc") != std::string::npos) {
+    if(mpi->mpiHead()) std::cout << "Parsing FC2s from QE." << std::endl;
     return QEParser::parsePhHarmonic(context);
   }
-  else {
+  // the jdftx filename is seedname.phononHsub
+  else if(fc2FileName.find("phononOmegaSq") != std::string::npos) {
+    if(mpi->mpiHead()) std::cout << "Parsing FC2s from JDFTx." << std::endl;
+    return JDFTxParser::parsePhHarmonic(context);
+  }
+  // otherwise try to parse as a QE 
+  else if (!context.getPhonopyDispFileName().empty()) {
+    if(mpi->mpiHead()) std::cout << "Parsing FC2s from Phonopy." << std::endl;
     return PhonopyParser::parsePhHarmonic(context);
+  }
+  else {
+    Error("Unrecognised FC2 format!\n"
+    "-- to use QE, supply a file with a .fc extension\n"
+    "-- to use JDFTx, supply a file with the .phononHsub extension\n"
+    "-- to use phonopy, set the phonopyDispFileName input variable and supply a .hdf5 file\n");
   }
 }
 

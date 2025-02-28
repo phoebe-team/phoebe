4c3ae8c7537a80db83e43445500e9cccf6fde3d9
jcoulter12 <jcoulter@flatironinstitute.org>
2025-02-28 08:31:23 -0500

    further addition of linewidth reconstruction input variable

--- src/bte/coupled_scattering_matrix.cpp
+++ src/bte/coupled_scattering_matrix.cpp
@@ -330,7 +330,9 @@ void CoupledScatteringMatrix::builder(std::shared_ptr<VectorBTE> linewidth,
   // use the off diagonals to calculate the linewidths,
   // to ensure the special eigenvectors can be found/preserve conservation of momentum
   // that might be ruined by the delta functions
-  reinforceLinewidths();
+  if(context.getReconstructLinewidths()) {
+    reinforceLinewidths();
+  }
 
   // TODO debug the "replaceLinewidths" function and use it instead
   // we place the linewidths back in the diagonal of the scattering matrix

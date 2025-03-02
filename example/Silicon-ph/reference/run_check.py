1d88f4257ea359e4e01490c5898e028c95d0c5ca
jcoulter12 <jcoulter@flatironinstitute.org>
2025-03-02 10:19:00 -0500

    try again to pin down issue with actions

--- example/Silicon-ph/reference/run_check.py
+++ example/Silicon-ph/reference/run_check.py
@@ -153,9 +153,9 @@ if __name__ == "__main__":
                 sys.exit(1)
             k1 = numpy.array(data1['energies'])
             k2 = numpy.array(data2['energies'])
-            diff = ((k1 - k2)/numpy.max(k1)).sum()
+            #diff = ((k1 - k2)/numpy.max(k1)).sum()
             diff = numpy.linalg.norm((k1[numpy.where(k1!=0)]- k2[numpy.where(k1!=0)])/k1[numpy.where(k1!=0)])
-            #if abs(diff) > tol:
+            if abs(diff) > tol:
                 print("new energies:",k1)
                 print("ref energies:",k2)
                 print("energies diff:",diff)
